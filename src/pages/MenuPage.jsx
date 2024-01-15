9ae7b37bb27c8275086a542cf3f595abaaf5a581
Kavishka Nimsara <nimsaragk.21@uom.lk>
2024-01-15 19:31:37 +0530

    menu

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -10,10 +10,10 @@ const MenuPage = () => {
   return (
    <div className='bg-secondary'>
     <div className='h-screen'>
-        <div className='flex justify-center px-12 pt-4 '> 
+        <div className='flex justify-center px-12 pt-8 '> 
             <img src={logoblack} alt="" />
         </div>   
-        <div className='flex-row gap-3 md:flex items-center justify-center py-6 md:py-20 px-8 bg-secondary'>
+        <div className='flex-row gap-3 md:flex items-center justify-center py-8 md:py-20 px-8 bg-secondary'>
             <MenuCard title="Breakfast" url={breakfastImg} time='08:00 - 10:00' destination="/breakfast" />
             <MenuCard title="Lunch" url={lunchImg} time='12:00 - 13:00'destination="/lunch" />
             <MenuCard title="Dinner" url={DinnerImg} time='18:00 - 21:00' destination="/dinner" />
